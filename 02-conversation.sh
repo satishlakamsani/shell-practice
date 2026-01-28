cfaa1811bbeeb790087df5f98a3c6e2be428d9e7
satishlakamsani <lakamsanisatish@gmail.com>
2026-01-28 10:48:49 +0530

    conversation added

---
+++ 02-conversation.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Suresh::Hello Ramesh,How are you?"
+echo "Ramesh::I am fine Suresh,What about you?"
+echo "Suresh::I am also fine.Thanks.What are you learning ramesh?"
+echo "Ramesh::I am learning shell.What about you?"
