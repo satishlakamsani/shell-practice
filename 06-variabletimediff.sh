8059df65108488345db45baf7a16358590af0ef4
satishlakamsani <lakamsanisatish@gmail.com>
2026-01-28 12:13:30 +0530

    date modification

--- 06-variabletimediff.sh
+++ 06-variabletimediff.sh
@@ -3,7 +3,7 @@ START_TIME=$(date +%s)
 
 echo "script started at:$START_TIME"
 sleep 10
-END_TIME=$(date+%s)
+END_TIME=$(date +%s)
 echo "script ended at:$END_TIME"
 TOTAL_TIME=$(($END_TIME - $START_TIME))
 echo "Script executed in $TOTAL_TIME seconds"
