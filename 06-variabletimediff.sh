786c5741497d2d0b21c30426c8b5f8d7bd32ba46
satishlakamsani <lakamsanisatish@gmail.com>
2026-01-28 12:06:21 +0530

    timm diff variable added

---
+++ 06-variabletimediff.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+START_TIME=$(date +%s)
+
+echo "script started at:$START_TIME"
+sleep 10
+END_TIME=$(date+%s)
+echo "script ended at:$END_TIME"
+TOTAL_TIME=$(($END_TIME - $START_TIME))
+echo "Script executed in $TOTAL_TIME seconds"
