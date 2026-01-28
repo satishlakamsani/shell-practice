68b49c09074fe28cb588a98940cd49dee71f0bef
satishlakamsani <lakamsanisatish@gmail.com>
2026-01-28 13:00:35 +0530

    special variables added

---
+++ 07-special-variables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+####Special variables ####
+echo "All args passed to scripts :$@"
+echo "No of args passed to script:$#"
+echo "Script name is :$0"
+echo "Present working directory is :$PWD"
+echo "Home directory of current user is :$HOME"
+echo "Current user is :$USER"
+echo "PID of current script is:$$"
+sleep 10 &
+echo "PID of last background process is:$!"
+echo "All arguments passed to script:$*"
