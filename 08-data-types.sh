365a045bdbcb320f8e34556f306dd88d895ee345
satishlakamsani <lakamsanisatish@gmail.com>
2026-01-28 13:27:58 +0530

    add data type variables

---
+++ 08-data-types.sh
@@ -0,0 +1,10 @@
+NUM1=100
+NUM2=SATISH
+SUM=$(($NUM1 + $NUM2))
+echo "Sum is: $SUM"
+
+#Array
+Fruits=("Mango","Apple","Bananana")
+echo "First fruit is:${Fruits[0]}"
+echo "Second fruit is ${Fruits[1]}"
+echo "Third fruit is ${Fruits[2]}"
