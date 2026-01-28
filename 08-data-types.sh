728b109533a29103d459514f0aa8241a727052b8
satishlakamsani <lakamsanisatish@gmail.com>
2026-01-28 13:40:11 +0530

    fruits all variable added newELY

--- 08-data-types.sh
+++ 08-data-types.sh
@@ -4,12 +4,9 @@ SUM=$(($NUM1 + $NUM2))
 echo "Sum is: $SUM"
 
 #Array
-Fruits=("Mango","Apple","Bananana")
+FRUITS=("Mango","Apple","Bananana")
 
-echo "Fruits are: ${Fruits[@]}"
-echo "First fruit is: ${Fruits[0]}"
-echo "Second fruit is: ${Fruits[1]}"
-echo "Third fruit is: ${Fruits[2]}"
+echo "Fruits are: ${FRUITS[@]}"
 echo "First Fruit is: ${FRUITS[0]}"
 echo "Second Fruit is: ${FRUITS[1]}"
 echo "Third Fruit is: ${FRUITS[2]}"
