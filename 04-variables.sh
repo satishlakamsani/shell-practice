707397cee86398a5b6350cd0519eab95a2b30063
satishlakamsani <lakamsanisatish@gmail.com>
2026-01-28 11:27:41 +0530

    variable $ added

---
+++ 04-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+PERSON1=$1
+PERSON2=$2
+echo "$PERSON1:: Hello $PERSON2.How are you?"
+echo "$PERSON2:: I am fine $PERSON1 .what about you?"
+echo "$PERSON1:: I am also fine.Thanks what are you doing $PERSON2?"
+echo "$PERSON2:: I am learning shell scripting.What about you $PERSON1?"
