c88d2fa02daabffac1f4e795f420f415a9229b3e
satishlakamsani <lakamsanisatish@gmail.com>
2026-01-28 11:16:22 +0530

    variablename added

---
+++ 03-variablename.sh
@@ -0,0 +1,7 @@
+#!/bin/bash/
+PERSON1=Trump #no spaces after =
+PERSON2=Putin
+echo "$PERSON1:: Hello $PERSON2.How are you?"
+echo "$PERSON2:: I am fine $PERSON1 .what about you?"
+echo "$PERSON1:: I am also fine.Thanks what are you doing $PERSON2?"
+echo "$PERSON2:: I am learning shell scripting.What about you $PERSON1?"
