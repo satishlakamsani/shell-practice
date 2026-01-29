bd39e8f0b496ec4ddcd4b871f9b745e62ccab328
satishlakamsani <lakamsanisatish@gmail.com>
2026-01-29 09:42:21 +0530

    variable timediff added

--- 06-variabletimediff.sh
+++ 06-variabletimediff.sh
@@ -13,5 +13,5 @@ echo "script begin time in seconds:$BEGIN_TIME"
 sleep 5
 FINISH_TIME=$(date +%s)
 echo "script ended at in seconds:$FINISH_TIME"
-ELAPSED_TIME=$(($FINISHED_TIME - $BEGIN_TIME))
+ELAPSED_TIME=$(($FINISH_TIME - $BEGIN_TIME))
 echo "Total script executed time is :$ELAPSED_TIME in seconds"
