dd6f6b983e7b227ecc46db56dfb45b8b3d23e8cb
satishlakamsani <lakamsanisatish@gmail.com>
2026-01-29 09:51:06 +0530

    revised special variables

--- 07-special-variables.sh
+++ 07-special-variables.sh
@@ -11,3 +11,12 @@ echo "PID of current script is:$$"
 sleep 10 &
 echo "PID of last background process is:$!"
 echo "All arguments passed to script:$*"
+
+####Revision ####
+echo "$@ is for passing all args."
+echo "$# is for counting no of args."
+echo "script name is $0."
+echo "present working directory is $PWD."
+echo "home directory of current user is $HOME."
+echo "current user is $USER."
+echo "PID of cuurent script is $$."
