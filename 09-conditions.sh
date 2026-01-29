c6eeb00196f63729d514188a8a6027a84d0f4e87
satishlakamsani <lakamsanisatish@gmail.com>
2026-01-29 10:25:51 +0530

    conditions added

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 NUMBER=$1 
+COUNT=$2
 #-gt - greatet than
 #-lt - less than
 #-eq - equal
@@ -12,3 +13,11 @@ elif [ $NUMBER -lt 20 ]; then
 else
    echo "$NUMBER is equal to 20"
 fi 
+
+if [ COUNT -gt 25]; then
+   echo "$COUNT is greater than 25"
+elif [ $COUNT -eq 25]: then
+   echo "$COUNT is equal to 25."
+else
+   echo "$COUNT is less than 25"
+fi
