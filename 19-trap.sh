3d99ab6c71be50a5908e621d9035d71d0f7194f6
satishlakamsani <lakamsanisatish@gmail.com>
2026-01-29 18:48:27 +0530

    trap enableing

--- 19-trap.sh
+++ 19-trap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e #ERR
-trap 'echo "There is a error in $LINENUMBER,Command:BASH_COMMAND"' ERR
+trap 'echo "There is a error in $LINENUMBER,Command:$BASH_COMMAND"' ERR
 echo "I am learning shell-script"
 echo "If I will learn in these phase definetely I will bacome good in shell-scripting"
 eccoo "How is my script"
