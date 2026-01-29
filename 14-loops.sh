0e227740942315588fa571a25e649ee5629a4004
satishlakamsani <lakamsanisatish@gmail.com>
2026-01-29 15:38:34 +0530

    loops parameter added

---
+++ 14-loops.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+USERID=$(id -u)
+echo  $USERID
+LOGS_FOLDER="/var/log/shell-script"
+LOGS_FILE="/vat/log/shell-script/$0.log"
+
+if [ $USERID -ne 0 ]; then
+   echo "please run this script as root user or using sudo" | tee -a $LOGS_FILE
+   exit 1
+fi
+mkdir -p $LOGS_FOLDER
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+      echo "$2 ... FAILURE"  |tee -a $LOGS_FILE
+      exit 1
+    else 
+    echo "$2 successfully ... SUCCESS" |tee -a $LOGS_FILE
+    fi
+          }
+    for package in $@ # sudo sh-loops.sh nginx mysql nodejs
+    do 
+    dnf install $package -y &>> $LOGS_FILE
+    VALIDATE $? "Installing $package"
+    done
