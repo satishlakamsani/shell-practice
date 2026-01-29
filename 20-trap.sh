54d1b8111cf8e1add0e90db7584810620618a179
satishlakamsani <lakamsanisatish@gmail.com>
2026-01-29 18:52:13 +0530

    trap added

---
+++ 20-trap.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -e  #this will check the errors if any command fails the script will exit immediately.
+
+trap 'echo "There is a error in $LINENUMBER,Command:$BASH_COMMAND"' ERR
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-script"
+LOGS_FILE="/var/log/shell-script/$0.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+#!/bin/bash
+
+if [ $USERID -ne 0 ]; then
+    echo -e "$R Please run this script with root user access $N" | tee -a $LOGS_FILE
+    exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+
+
+for package in $@ # sudo sh 14-loops.sh nginx mysql nodejs
+do
+    dnf list installed $package &>>$LOGS_FILE
+    if [ $? -ne 0 ]; then
+        echo "$package not installed, installing now"
+        dnf install $package -y &>>$LOGS_FILE
+        
+    else
+        echo -e "$package already installed ... $Y SKIPPING $N"
+    fi
+done
