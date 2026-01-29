632218c8cd0e9ed52a2bc52ef4b3bd8762d86093
satishlakamsani <lakamsanisatish@gmail.com>
2026-01-29 18:38:55 +0530

    trap enableing

---
+++ 19-trap.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -e
+echo "I am learning shell-script"
+echo "If I will learn in these phase definetely I will bacome good in shell-scripting"
+eccoo "How is my script"
+echo "This is my latest trick"
