d79ae07a012eafcf1dcf480560c92ca006136075
satishlakamsani <lakamsanisatish@gmail.com>
2026-02-01 12:50:53 +0530

    added files

--- 21-shell-verification.sh
+++ 21-shell-verification.sh
@@ -3,6 +3,10 @@ USERID=$(id -u)
 PERSON1=$1
 PERSON2=$2
 echo "USERID::$USERID"
+echo "$0 gives current directory"
+echo "PWD::$PWD"
+echo "Total parameters are::$@"
+echo "The number of parameters is $#"
 echo "$1:: Hello $2 How are you?"
 for instance in $@
 do
