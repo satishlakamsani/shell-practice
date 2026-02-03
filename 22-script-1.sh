c330e882422bc4c28f03f9950d38cbf843c406f2
satishlakamsani <lakamsanisatish@gmail.com>
2026-02-03 10:36:55 +0530

    script source shell added

---
+++ 22-script-1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+COUNTRY=INDIA
+
+echo "I am in $COUNTRY"
+echo "PID of SCRIPT-1:$$"
+
+sh 23-script-2.sh
---
+++ 23-script-2.sh
@@ -0,0 +1,3 @@
+#!/bin/bash 
+echo "I am in $COUNTRY"
+echo "PID of SCRIPT-2:$$"
