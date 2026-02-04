09890cb77bf35adcbdbcc42eac971419fa130d54
satishlakamsani <lakamsanisatish@gmail.com>
2026-02-04 16:09:30 +0530

    backup file added

--- 26-backup.sh
+++ 26-backup.sh
@@ -7,6 +7,14 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
+log(){
+    echo -e "$(date "+%Y-%m-%d %H:%M:%S") |$1" |tee -a $LOGS_FILE
+}
+
+SOURCE_DIR=$1
+DEST_DIR=$2
+DAYS=${3:-14} #Default it takes 14 days if you not mention
+
 if [ $USERID -ne 0 ]; then
     echo -e "$R Please run this script with root user access $N" 
     exit 1
@@ -21,3 +29,11 @@ USAGE(){
 if [ $# -lt 2 ]; then
   USAGE
 fi
+
+if [ ! -d $SOURCE_DIR ]; then
+ log "$R Source Directory: $SOURCE_DIR does not exist $N"
+ exit 1
+fi
+
+if [ !-d $DEST_DIR ]; then
+log "$R Destination Directory: $DEST_DIR does not exist $N"
