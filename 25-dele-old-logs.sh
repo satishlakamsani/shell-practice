1af1d82f62b185b08ec72b14a55d80bfa4a32c98
satishlakamsani <lakamsanisatish@gmail.com>
2026-02-04 13:39:22 +0530

    delete old files added

--- 25-dele-old-logs.sh
+++ 25-dele-old-logs.sh
@@ -9,4 +9,8 @@ LOGS_FILE="$LOGS_DIR/$$0.log"
 
 if [ ! -d $LOGS_DIR ]; then
    echo -e "$LOGS_DIR does not exist"
+   exit 1
 fi
+
+FILES_TO_DELETE=$(find . -name "*.log" -type f -mtime +14)
+echo "$FILES_TO_DELETE"
