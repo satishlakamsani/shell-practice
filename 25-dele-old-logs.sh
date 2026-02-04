178230c92ff948a9e4031b62815c3f8f3d409f3a
satishlakamsani <lakamsanisatish@gmail.com>
2026-02-04 13:41:04 +0530

    delete old files added

--- 25-dele-old-logs.sh
+++ 25-dele-old-logs.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
+
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
 LOGS_DIR=/home/ec2-user/app-logs
-LOGS_FILE="$LOGS_DIR/$$0.log"
+LOGS_FILE="$LOGS_DIR/$0.log"
 
 if [ ! -d $LOGS_DIR ]; then
-   echo -e "$LOGS_DIR does not exist"
-   exit 1
+    echo -e "$LOGS_DIR does not exitst"
+    exit 1
 fi
 
-FILES_TO_DELETE=$(find . -name "*.log" -type f -mtime +14)
-echo "$FILES_TO_DELETE"
+FILES_TO_DELETE=$(find $LOGS_DIR -name "*.log" -mtime +14)
+# echo "$FILES_TO_DELETE"
+
+while IFS= read -r filepath; do
+  # Process each line here
+  echo "Deleting file: $filepath"
+  rm -f $filepath
+  echo "Deleted file: $filepath"
+done <<< $FILES_TO_DELETE
