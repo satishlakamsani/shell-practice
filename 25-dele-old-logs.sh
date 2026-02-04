9549aa85c42fccda4b1277e13b6b8e4b913ac13d
satishlakamsani <lakamsanisatish@gmail.com>
2026-02-04 13:36:37 +0530

    delete old files added

---
+++ 25-dele-old-logs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_DIR=/home/ec2-user/app-logs
+LOGS_FILE="$LOGS_DIR/$$0.log"
+
+if [ ! -d $LOGS_DIR ]; then
+   echo -e "$LOGS_DIR does not exist"
+fi
