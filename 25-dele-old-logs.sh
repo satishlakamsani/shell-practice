23f1d7367f385afeefec7c11b8c72229654373b8
satishlakamsani <lakamsanisatish@gmail.com>
2026-02-04 13:50:53 +0530

    delete old files added

--- 25-dele-old-logs.sh
+++ 25-dele-old-logs.sh
@@ -14,7 +14,7 @@ if [ ! -d $LOGS_DIR ]; then
 fi
 
 FILES_TO_DELETE=$(find $LOGS_DIR -name "*.log" -mtime +14)
-# echo "$FILES_TO_DELETE"
+ echo "$FILES_TO_DELETE"
 
 #while IFS= read -r filepath; do
   # Process each line here
