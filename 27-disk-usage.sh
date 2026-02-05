05dde2b326feb1ce0d0ebfd0c3be225e3e4888e7
satishlakamsani <lakamsanisatish@gmail.com>
2026-02-05 14:36:03 +0530

    mail added

--- 27-disk-usage.sh
+++ 27-disk-usage.sh
@@ -25,4 +25,5 @@ do
 done <<< $DISK_USAGE
 
 echo -e "$MESSAGE"
-sh mail.sh "lakamsanisatish295@gmail.com" "High Disk Usage Alert on $IP_ADDRESS" "$MESSAGE" "HIGH_DISK_USAGE" "$IP_ADDRESS" "DevOps Team"
+
+sh mail.sh "lakamsanisatish@gmail.com" "High Disk Usage Alert on $IP_ADDRESS" "$MESSAGE" "HIGH_DISK_USAGE" "$IP_ADDRESS" "DevOps Team"
