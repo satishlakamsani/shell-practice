9d57a5ed9787d986989fa28a38e68fb892969e85
satishlakamsani <lakamsanisatish@gmail.com>
2026-02-05 12:28:56 +0530

    disk usage added

--- 27-disk-usage.sh
+++ 27-disk-usage.sh
@@ -5,7 +5,7 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 MESSAGE=""
-IP_ADDRESS=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
+#IP_ADDRESS=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
 
 log(){
     echo -e "$(date "+%Y-%m-%d %H:%M:%S") | $1"
